4aa4bd2ef5df51ed3cc8ce7ac95c6f53991494f9
gpffhwodnjs <gpffhwodnjs@gmail.com>
2023-07-26 17:31:28 +0900

    첫 커밋

--- src/main/java/ssg/com/a/util/NetflixUtil.java
+++ src/main/java/ssg/com/a/util/NetflixUtil.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.math.RoundingMode;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -217,4 +218,103 @@ public class NetflixUtil {
         Double rating = Double.parseDouble(num_str);
     	return rating;
     }
+
+    public static List<NetflixContentDto> searchNetflixMovie(String query) throws Exception {
+        String apiKey = "d5aa3423c1d19e90331f7ffda7962079";
+        String language = "ko-KR";
+        String netflixId = "8";
+        int currentPage = 1;
+        int totalPages = 1;
+        
+        List<NetflixContentDto> contentList = new ArrayList<>();
+
+        do {
+            URL url = new URL("https://api.themoviedb.org/3/search/movie"
+                    + "?api_key=" + apiKey
+                    + "&with_watch_providers=" + netflixId
+                    + "&language=" + language
+                    + "&query=" + URLEncoder.encode(query, "UTF-8")
+                    + "&page=" + currentPage);
+
+            BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            String result = bf.readLine();
+            bf.close();
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+            JSONArray list = (JSONArray) jsonObject.get("results");
+
+            if (currentPage == 1) {
+                // 첫 페이지 검색 결과에서 총 페이지 수를 얻어옵니다.
+                totalPages = Integer.parseInt(jsonObject.get("total_pages").toString());
+            }
+
+            for (Object obj : list) {
+                JSONObject movie = (JSONObject) obj;
+                // JSON에서 NetflixContentDto 객체를 추출하여 contentList에 추가합니다.
+                Long id = (Long) movie.get("id"); // id는 Long형태
+			    String title = (String) movie.get("title");
+			    String overview = (String) movie.get("overview");
+			    String posterPath = (String) movie.get("poster_path"); // https://image.tmdb.org/t/p/w500" + posterPath
+			    Double popularity = Double.parseDouble(movie.get("popularity").toString()); 
+			    String releaseDate = (String) movie.get("release_date");
+                NetflixContentDto content = new NetflixContentDto(id, title, overview, posterPath, popularity, releaseDate);
+                contentList.add(content);
+            }
+
+            currentPage++; // 다음 페이지로 이동합니다.
+        } while (currentPage <= totalPages);
+
+        return contentList;
+    }
+    
+    public static List<NetflixTvDto> searchNetflixTV(String query) throws Exception {
+        String apiKey = "d5aa3423c1d19e90331f7ffda7962079";
+        String language = "ko-KR";
+        String netflixId = "8";
+        int currentPage = 1;
+        int totalPages = 1;
+
+        List<NetflixTvDto> contentList = new ArrayList<>();
+
+        do {
+            URL url = new URL("https://api.themoviedb.org/3/search/tv"
+                    + "?api_key=" + apiKey
+                    + "&with_watch_providers=" + netflixId
+                    + "&language=" + language
+                    + "&query=" + URLEncoder.encode(query, "UTF-8")
+                    + "&page=" + currentPage);
+
+            BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            String result = bf.readLine();
+            bf.close();
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+            JSONArray list = (JSONArray) jsonObject.get("results");
+
+            if (currentPage == 1) {
+                // 첫 페이지 검색 결과에서 총 페이지 수를 얻어옵니다.
+                totalPages = Integer.parseInt(jsonObject.get("total_pages").toString());
+            }
+
+            for (Object obj : list) {
+                JSONObject tvShow = (JSONObject) obj;
+                // JSON에서 NetflixTvDto 객체를 추출하여 contentList에 추가합니다.
+                Long id = (Long) tvShow.get("id");
+                String title = (String) tvShow.get("name");
+                String overview = (String) tvShow.get("overview");
+                String posterPath = (String) tvShow.get("poster_path");
+                Double popularity = Double.parseDouble(tvShow.get("popularity").toString());
+                String releaseDate = (String) tvShow.get("first_air_date");
+                NetflixTvDto content = new NetflixTvDto(id, title, overview, posterPath, popularity, releaseDate);
+                contentList.add(content);
+            }
+
+            currentPage++; // 다음 페이지로 이동합니다.
+        } while (currentPage <= totalPages);
+
+        return contentList;
+    }
+
 }
